4e4b4517df1f1745d94dea69a1e76005052527ba
hyu7000 <p.joyplace@gmail.com>
2024-03-16 02:36:44 +0900

    update configuration and generate detail comment

--- extension.js
+++ extension.js
@@ -4,6 +4,30 @@ const vscode = require('vscode');
 const fs = require('fs');
 const path = require('path');
 
+const {
+    C_FILE_COMMENT,
+    HEADER_FILE_COMMENT,
+    INCLUDES_COMMENT,
+    MACRO_COMMENT,
+    GLOBAL_VAR_COMMENT,
+    DATA_STRUCTURE_COMMENT,
+    FUNCTION_PROTOTYPES_COMMENT,
+    INTERRUPT_COMMENT,
+    PRIVATE_FUNCTION_COMMENT,
+    EXTERN_FUNCTION_COMMENT,
+
+    DEFAULT_COMMENTS,
+    DEFAULT_HEADER_FILE_COMMENT_TYPES,
+    DEFAULT_C_FILE_COMMENT_TYPES,
+    DEFAULT_SETTING_DATA
+} = require('./js/default_settings.js');
+
+let commentReplacements = {};
+
+let settingData = {};
+
+let is_commnent_saved = false;
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -15,24 +39,24 @@ function activate(context) {
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
 
-    // 주석 생성 커맨드 초기화
-    let gen_comment = initGenCommand()
-    context.subscriptions.push(gen_comment);
+    // 저장된 데이터 불러오기
+    checkSavedData(context);
+
+    // 디테일 주석 생성 커맨드 초기화
+    let gen_detail_comment = initGenDetailCommand();
+    context.subscriptions.push(gen_detail_comment);
+
+    // 일반 주석 생성 커맨드 초기화
+    let gen_normal_comment = initGenNormalCommand();
+    context.subscriptions.push(gen_normal_comment);
 
     // 웹뷰 초기화
-    let open_page = initWebView(context)
+    let open_page = initWebView(context);
     context.subscriptions.push(open_page);
 
-    // 상태 바 항목 생성
-    let statusBarBtn = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-    statusBarBtn.command = 'comment-maker.GenerateComment'; // 이 항목을 클릭했을 때 실행할 명령어
-    statusBarBtn.text = "$(comment) Add Comment"; // 버튼에 표시될 텍스트 및 아이콘 (예: Octicon 아이콘 사용)
-    statusBarBtn.tooltip = "Generate Comment"; // 버튼에 마우스를 올렸을 때 표시될 툴팁
-
-    statusBarBtn.show(); // 상태 바 항목을 표시
-    context.subscriptions.push(statusBarBtn);
-
-    console.log('추가됨');
+    // 상태 바에 디테일 주석 버튼 항목 생성
+    let genDetailCommentBtn = initGenDetailCommentBtn();
+    context.subscriptions.push(genDetailCommentBtn);
 }
 
 // This method is called when your extension is deactivated
@@ -97,6 +121,39 @@ function getTotalLines() {
 }
 
 /**
+ * 주어진 객체의 각 value를 vscode 파일에 삽입합니다.
+ * @param {Object} obj 삽입할 value 값을 가진 객체
+ * @param {number} startLine 삽입을 시작할 라인 번호
+ */
+function generateNormalAllComment(obj, startLine) {
+    let fullText = ''; // 모든 텍스트를 여기에 저장할 예정
+
+    // 모든 key-value 쌍에 대해 반복
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const values = obj[key]; // 현재 key의 value를 가져옴
+            if (Array.isArray(values)) {
+                // value 값이 배열인 경우, 각 항목을 분리하여 삽입
+                values.forEach((value, index) => {
+                    // 현재 value 값을 fullText에 추가
+                    fullText += value + '\n';
+                    if (index < values.length - 1) { // 마지막 항목이 아니라면 두 줄 띄우기
+                        // fullText에 두 줄을 추가
+                        fullText += '\n\n';
+                    }
+                });
+            } else {
+                // value 값이 단일 문자열인 경우 바로 추가
+                fullText += values + '\n\n\n'; // 문자열 끝에 두 줄 띄우기를 추가
+            }
+        }
+    }
+
+    // 이제 fullText에 모든 주석이 저장되어 있으므로, insertTextFromLine를 한 번만 호출합니다.
+    insertTextFromLine(startLine, fullText);
+}
+
+/**
  * 현재 선택된 파일에 특정 라인부터 시작하여 문자열을 입력합니다.
  * 문자열 내의 \n 기호를 기준으로 여러 라인에 걸쳐 입력할 수 있습니다.
  * @param {number} startLine 시작 라인 인덱스 (0부터 시작)
@@ -152,6 +209,44 @@ function findFirstHashLineIndex() {
     return -1; // '#'으로 시작하는 라인을 찾지 못한 경우 -1 반환
 }
 
+// 현재 활성화된 탭의 확장자 반환
+function getCurrentFileExtension() {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        const filePath = editor.document.uri.path;
+        const extensionMatch = filePath.match(/\.([0-9a-z]+)(?:[\?#]|$)/i); // 파일 확장자를 정규식을 사용하여 추출
+        if (extensionMatch) {
+            const extension = extensionMatch[0]; // '.c', '.h' 등과 같은 형태로 추출됩니다.
+            console.log(extension); // 확장자 출력
+            return extension; // 확장자 반환
+        } else {
+            console.log('No file extension found');
+            return ''; // 확장자를 찾을 수 없는 경우
+        }
+    } else {
+        console.log('No active editor');
+        return ''; // 에디터가 활성화되어 있지 않은 경우
+    }
+}
+
+function getCurrentFileName() {
+    // 현재 활성화된 텍스트 편집기를 가져옵니다.
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        console.log('No active editor');
+        return ''; // 활성 편집기가 없다면 빈 문자열 반환
+    }
+
+    // 현재 편집 중인 문서의 URI를 가져옵니다.
+    const documentUri = editor.document.uri;
+    // URI 객체에서 경로 부분만 추출합니다.
+    const filePath = documentUri.fsPath;
+
+    // 경로에서 파일 이름을 추출합니다. (확장자 포함)
+    const fileName = path.basename(filePath);
+
+    return fileName; // 파일명 반환
+}
 
 // match_info 를 매개변수로 받고, 주석을 생성하는 함수
 function generateCommentFunction(match_info) {
@@ -197,8 +292,35 @@ function generateCommentComponent() {
     return comment;
 }
 
+function checkSavedData(context) {
+    is_commnent_saved = context.globalState.get('is_commnent_saved', false);
+
+    if(is_commnent_saved) {
+        commentReplacements = context.globalState.get('saved_comment',      DEFAULT_COMMENTS);
+        settingData         = context.globalState.get('saved_setting_data', DEFAULT_SETTING_DATA);
+    }
+    else {
+        commentReplacements = DEFAULT_COMMENTS;
+        settingData         = DEFAULT_SETTING_DATA;
+    }
+}
+
+function replaceHtmlCode(htmlCode, detectCode, replace) {
+    // <!--C_FILE_COMMENT--> 형태의 주석만 매칭하기 위한 정규 표현식
+    const pattern = new RegExp(`<!--${detectCode}-->`, 'g');
+    return htmlCode.replace(pattern, replace);
+}
+
+function replaceAllComments(htmlCode, replacements) {
+    let modifiedHtml = htmlCode;
+    for (const [detectCode, replace] of Object.entries(replacements)) {
+        modifiedHtml = replaceHtmlCode(modifiedHtml, detectCode, replace);
+    }
+    return modifiedHtml;
+}
+
 function initWebView(context) {
-    let web_view = vscode.commands.registerCommand('yourExtension.showWebView', function() {
+    let web_view = vscode.commands.registerCommand('comment-maker.Settings', function() {
         const panel = vscode.window.createWebviewPanel(
             'Settings Comment Generation', // 웹뷰 유형을 식별하는 문자열
             'Settings Comment Generation', // 웹뷰 타이틀
@@ -224,19 +346,56 @@ function initWebView(context) {
         
         // Tab1 초기화
         const initialContentPath = path.join(context.extensionPath, 'pages', 'Tab1.html');
-        const initialContent = fs.readFileSync(initialContentPath, 'utf8');
-        panel.webview.postMessage({ command: 'updateContent', content: initialContent });
+        let initialContent = fs.readFileSync(initialContentPath, 'utf8');
+        initialContent = replaceAllComments(initialContent, commentReplacements);
+        panel.webview.postMessage({ command: 'updateContent_Tab1', content: initialContent });
 
         // 메시지 수신 리스너 설정
         panel.webview.onDidReceiveMessage(message => {
-            switch (message.command) {
-                case 'saveText':
-                    console.log(message.text);
-                    break;
+            switch (message.command) {   
                 case 'requestContent':
-                    const tabHtmlPath = path.join(context.extensionPath, 'pages', `${message.tabName}.html`);
-                    const tabContent = fs.readFileSync(tabHtmlPath, 'utf8');
-                    panel.webview.postMessage({ command: 'updateContent', content: tabContent });
+                    let tabHtmlPath = path.join(context.extensionPath, 'pages', `${message.tabName}.html`);
+                    let tabContent = fs.readFileSync(tabHtmlPath, 'utf8');
+
+                    if (message.tabName == 'Tab1') {
+                        // commentReplacements 객체를 사용하여 HTML 내의 모든 주석을 한 번에 교체
+                        tabContent = replaceAllComments(tabContent, commentReplacements);
+                        panel.webview.postMessage({ command: 'updateContent_Tab1', content: tabContent });
+                    }
+                    else if (message.tabName == 'Tab2') {
+                        tabContent = replaceAllComments(tabContent, settingData);
+                        panel.webview.postMessage({ command: 'updateContent', content: tabContent });
+                    }   
+                    else {
+                        panel.webview.postMessage({ command: 'updateContent', content: tabContent });
+                    }
+
+                    break;
+
+                case 'request_default_comments':
+                    panel.webview.postMessage({ command: 'response_default_comments', default_comments: DEFAULT_COMMENTS });
+                    break;
+
+                case 'save_comment':        
+                    context.globalState.update('saved_comment', message.comment).then(() => {
+                        // 데이터 저장이 성공적으로 완료된 후 실행할 코드
+                        panel.webview.postMessage({ command: 'saved_successfully'});
+                        commentReplacements = message.comment;
+                    }).catch((error) => {
+                        // 데이터 저장 중 오류가 발생했을 때 실행할 코드
+                        console.error('Error saving the comment:', error);
+                    });
+                    break;
+                    
+                case 'save_setting_data':
+                    context.globalState.update('saved_setting_data', message.setting).then(() => {
+                        // 데이터 저장이 성공적으로 완료된 후 실행할 코드
+                        panel.webview.postMessage({ command: 'saved_successfully'});
+                        settingData = message.setting;
+                    }).catch((error) => {
+                        // 데이터 저장 중 오류가 발생했을 때 실행할 코드
+                        console.error('Error saving the comment:', error);
+                    });
                     break;
             }
         }, undefined, context.subscriptions);
@@ -245,8 +404,8 @@ function initWebView(context) {
     return web_view;
 }
 
-function initGenCommand() {
-    let gen_comment = vscode.commands.registerCommand('comment-maker.GenerateComment', function() {
+function initGenDetailCommand() {
+    let gen_comment = vscode.commands.registerCommand('comment-maker.GenerateDetailComment', function() {
         let line_index = getTotalLines();
 
         while (line_index >= 0) {
@@ -273,6 +432,105 @@ function initGenCommand() {
     });
 
     return gen_comment;
+}
+
+function filterCommentsByTypes(defaultComments, commentTypes) {
+    let filteredComments = {};
+    // commentTypes 배열을 순회하면서, 각 키에 대응하는 defaultComments의 값을 새 객체에 추가
+    commentTypes.forEach(type => {
+        if (defaultComments[type]) { // 해당 타입의 코멘트가 존재한다면
+            filteredComments[type] = defaultComments[type];
+        }
+    });
+    return filteredComments;
+}
+
+function replaceStringAndFillWithSpaces(originalString, searchString, replaceString) {
+    // 다중라인 처리를 위해 originalString을 라인별로 분할합니다.
+    const lines = originalString.split('\n');
+    const resultLines = [];
+
+    lines.forEach(line => {
+        // 각 라인에 대해서 searchString의 시작 위치를 찾습니다.
+        const indexToReplace = line.indexOf(searchString);
+        if (indexToReplace === -1) {
+            // searchString이 없는 경우 원본 라인을 그대로 사용합니다.
+            resultLines.push(line);
+            return;
+        }
+
+        // '**' 문자열의 시작 위치를 찾습니다.
+        const indexDoubleAsterisks = line.indexOf('**', indexToReplace);
+
+        // searchString을 replaceString으로 대체하고, 나머지 부분은 공백으로 채웁니다.
+        let newLine = line.substring(0, indexToReplace) + 
+                      replaceString + 
+                      ' '.repeat(Math.max(0, indexDoubleAsterisks - indexToReplace - replaceString.length));
+
+        if (indexDoubleAsterisks !== -1) {
+            // '**' 이후의 문자열을 원본에서 복사합니다.
+            newLine += line.substring(indexDoubleAsterisks);
+        }
+
+        // 결과 라인 추가
+        resultLines.push(newLine);
+    });
+
+    // 수정된 라인들을 다시 하나의 문자열로 병합합니다.
+    return resultLines.join('\n');
+}
+
+
+
+function getCurrentDateString() {
+    const date = new Date(); // 현재 날짜와 시간
+
+    const year = date.getFullYear(); // 년도
+    const month = ('0' + (date.getMonth() + 1)).slice(-2); // 월 (getMonth()는 0부터 시작하기 때문에 +1)
+    const day = ('0' + date.getDate()).slice(-2); // 일 (getDate()는 현재 날짜)
+
+    return `${year}.${month}.${day}`; // 형식에 맞게 반환
+}
+
+function initGenNormalCommand() {
+    let gen_normal_comment = vscode.commands.registerCommand('comment-maker.GenerateNormalComment', function() {
+        let temp_commentReplacements = {};
+
+        if (getCurrentFileExtension() == '.c')
+        {
+            temp_commentReplacements = filterCommentsByTypes(DEFAULT_COMMENTS, DEFAULT_C_FILE_COMMENT_TYPES);
+            temp_commentReplacements['C_FILE_COMMENT'] = replaceStringAndFillWithSpaces(temp_commentReplacements['C_FILE_COMMENT'], '{Project_name}', settingData['Project_name']);
+            temp_commentReplacements['C_FILE_COMMENT'] = replaceStringAndFillWithSpaces(temp_commentReplacements['C_FILE_COMMENT'], '{Reviser}',      settingData['Reviser']);
+            temp_commentReplacements['C_FILE_COMMENT'] = replaceStringAndFillWithSpaces(temp_commentReplacements['C_FILE_COMMENT'], '{0000.00.00}',   getCurrentDateString());
+            temp_commentReplacements['C_FILE_COMMENT'] = replaceStringAndFillWithSpaces(temp_commentReplacements['C_FILE_COMMENT'], '{FileName}',     getCurrentFileName());
+            generateNormalAllComment(temp_commentReplacements, 0);
+        }
+        else if(getCurrentFileExtension() == '.h')
+        {
+            temp_commentReplacements = filterCommentsByTypes(DEFAULT_COMMENTS, DEFAULT_HEADER_FILE_COMMENT_TYPES);
+            temp_commentReplacements['HEADER_FILE_COMMENT'] = replaceStringAndFillWithSpaces(temp_commentReplacements['HEADER_FILE_COMMENT'], '{Project_name}', settingData['Project_name']);
+            temp_commentReplacements['HEADER_FILE_COMMENT'] = replaceStringAndFillWithSpaces(temp_commentReplacements['HEADER_FILE_COMMENT'], '{Reviser}',      settingData['Reviser']);
+            temp_commentReplacements['HEADER_FILE_COMMENT'] = replaceStringAndFillWithSpaces(temp_commentReplacements['HEADER_FILE_COMMENT'], '{0000.00.00}',   getCurrentDateString());
+            temp_commentReplacements['HEADER_FILE_COMMENT'] = replaceStringAndFillWithSpaces(temp_commentReplacements['HEADER_FILE_COMMENT'], '{FileName}',     getCurrentFileName());
+            generateNormalAllComment(temp_commentReplacements, 0);
+        }
+
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Completed add a comment');
+    });
+
+    return gen_normal_comment;
+}
+
+function initGenDetailCommentBtn() {
+    let genDetailCommentBtn = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    genDetailCommentBtn.command = 'comment-maker.GenerateDetailComment'; // 이 항목을 클릭했을 때 실행할 명령어
+    genDetailCommentBtn.text = "$(comment) Add Comment"; // 버튼에 표시될 텍스트 및 아이콘 (예: Octicon 아이콘 사용)
+    genDetailCommentBtn.tooltip = "Generate Comment"; // 버튼에 마우스를 올렸을 때 표시될 툴팁
+
+    genDetailCommentBtn.show(); // 상태 바 항목을 표시
+
+    return genDetailCommentBtn;
 }
 
 module.exports = {
--- js/webview_script.js
+++ js/webview_script.js
@@ -4,10 +4,6 @@ const vscode = acquireVsCodeApi();
     // 페이지가 로드되면, 이벤트 리스너를 추가합니다.
     document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('add-btn').addEventListener('click', () => {
-            const textToSave = document.getElementById('header_file').value;
-            const message = { command: 'saveText', text: textToSave };
-            // VS Code API를 통해 메시지를 전송합니다.
-            vscode.postMessage(message);
         });
     });
 })();
@@ -32,7 +28,6 @@ function addComment() {
     content.insertBefore(newComment, btnsContainer); // 새 텍스트 에어리어를 'add-btn' 바로 앞에 삽입
 }
 
-
 function openTab(evt, tabName) {
     // 모든 탭 버튼의 'active' 클래스를 제거합니다.
     var tabbuttons = document.getElementsByClassName("tab-button");
@@ -52,6 +47,100 @@ function autoResizeTextarea(textarea) {
     textarea.style.height = textarea.scrollHeight + 'px'; // 실제 스크롤 높이를 높이로 설정
 }
 
+function showToast() {
+    var toast = document.getElementById("toast");
+    toast.classList.add("show");
+    setTimeout(function(){ toast.classList.remove("show"); }, 3000); // 3초 후에 메시지 사라짐
+}
+
+function saveComment() {
+    // 비동기 작업을 위해 Promise를 사용
+    return new Promise((resolve, reject) => {
+        // 서버에 DEFAULT_COMMENTS 데이터 요청
+        vscode.postMessage({ command: 'request_default_comments' });
+
+        // 메시지 응답 리스너
+        const messageHandler = event => {
+            const message = event.data; // 수신된 메시지
+            if (message.command === 'response_default_comments') {
+                // 이벤트 리스너를 제거
+                window.removeEventListener('message', messageHandler);
+
+                // Promise를 해결하고 받은 데이터로 작업을 계속
+                resolve(message.default_comments);
+            }
+        };
+
+        // 이벤트 리스너를 추가
+        window.addEventListener('message', messageHandler);
+
+        // 설정한 시간 내에 응답이 없는 경우, Promise를 거부
+        setTimeout(() => {
+            window.removeEventListener('message', messageHandler);
+            reject(new Error('Response timeout'));
+        }, 5000); // 예: 5초 타임아웃
+    }).then(default_comments => {
+        // 서버로부터 받은 DEFAULT_COMMENTS를 사용하여 comment_data 생성
+        const comment_data = createUpdatedCommentsObject(default_comments);
+
+        // 최종적으로 수정된 comment_data를 서버로 전송
+        vscode.postMessage({ command: 'save_comment', comment: comment_data });
+    }).catch(error => {
+        console.error('Failed to receive default comments:', error);
+    });
+}
+
+function getSettingsData() {
+    // 설정 항목을 저장할 객체 초기화
+    const settings = {};
+
+    // 'setting-item' 클래스를 가진 모든 요소를 선택
+    const settingItems = document.querySelectorAll('.setting-item');
+
+    // 각 설정 항목을 순회하면서, id와 해당 값으로 객체를 구성
+    settingItems.forEach(item => {
+        // 각 setting-item 내의 input 요소 찾기
+        const input = item.querySelector('input[type="text"]');
+        // input 요소의 id와 값으로 settings 객체를 업데이트
+        if (input) {
+            settings[input.id] = input.value;
+        }
+    });
+
+    // 설정 값을 저장하거나 후속 처리
+    console.log(settings); // 콘솔에 결과 출력, 실제 사용 시 이 부분에 후속 처리 코드를 추가할 수 있음
+    return settings; // 필요에 따라 설정 객체를 반환
+}
+
+function saveSettings() {
+    const settingData = getSettingsData();
+
+    vscode.postMessage({ command: 'save_setting_data', setting: settingData });
+}
+
+
+function createUpdatedCommentsObject(default_comment) {
+    // 새로운 객체를 초기화합니다.
+    const updatedComments = {};
+
+    // default_comment 객체의 각 항목에 대해 반복합니다.
+    Object.keys(default_comment).forEach(key => {
+        // 각 키에 해당하는 textarea 요소를 찾습니다.
+        const textarea = document.getElementById(key);
+
+        // 해당 textarea가 존재하면, 그 값을 사용하여 새 객체에 추가합니다.
+        if (textarea) {
+            updatedComments[key] = textarea.value;
+        } else {
+            // textarea가 존재하지 않으면, 기본값을 사용합니다.
+            updatedComments[key] = default_comment[key];
+        }
+    });
+
+    // 수정된 코멘트 객체를 반환합니다.
+    return updatedComments;
+}
+
 // 메시지 수신 리스너
 window.addEventListener('message', event => {
     const message = event.data; // 수신된 메시지
@@ -59,6 +148,18 @@ window.addEventListener('message', event => {
         case 'updateContent':
             // .content 영역에 수신된 HTML 컨텐츠로 업데이트
             document.querySelector('.content').innerHTML = message.content;
+            break;
+
+        case 'updateContent_Tab1':
+            document.querySelector('.content').innerHTML = message.content;
+            const textareas = document.getElementsByClassName('comment_textarea');
+            for (let i = 0; i < textareas.length; i++) {
+                autoResizeTextarea(textareas[i]);
+            }
+            break;
+
+        case 'saved_successfully':            
+            showToast();
             break;
     }
 });
