99548981acac33999744932c98b71b72562a772b
hyu7000 <p.joyplace@gmail.com>
2024-03-16 10:40:16 +0900

    update decoration

--- extension.js
+++ extension.js
@@ -57,10 +57,70 @@ function activate(context) {
     // 상태 바에 디테일 주석 버튼 항목 생성
     let genDetailCommentBtn = initGenDetailCommentBtn();
     context.subscriptions.push(genDetailCommentBtn);
+
+    // 주석 데코레이션 설정
+    initDecorations(context);
+    
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
+
+// Decorations 업데이트 로직을 별도의 함수로 분리
+function updateAtWordDecorations(activeEditor, decorationType) {
+    if (!activeEditor) {
+        return;
+    }
+
+    const regEx = /\/\*S[\s\S]*?\*\//g; // /*S ... */에 대한 정규 표현식
+    const text = activeEditor.document.getText();
+    const atWordDecorations = [];
+    let match;
+
+    while ((match = regEx.exec(text))) {
+        const subText = match[0];
+        let lastIndex = 0;  // 마지막으로 찾은 인덱스를 추적
+        const subMatches = subText.match(/@[\w]+/g);  // @로 시작하는 단어에 대한 정규 표현식
+    
+        if (subMatches) {
+            subMatches.forEach(matchStr => {  // 'match' 대신 'matchStr'로 이름 변경하여 혼동 방지
+                const matchIndex = subText.indexOf(matchStr, lastIndex);  // 마지막으로 찾은 위치 이후로부터 시작
+                const startPos = activeEditor.document.positionAt(match.index + matchIndex);
+                const endPos = activeEditor.document.positionAt(match.index + matchIndex + matchStr.length);
+                const decoration = { range: new vscode.Range(startPos, endPos) };
+                atWordDecorations.push(decoration);
+                lastIndex = matchIndex + matchStr.length;  // 마지막으로 찾은 인덱스 업데이트
+            });
+        }
+    }
+
+    activeEditor.setDecorations(decorationType, atWordDecorations);
+}
+
+function initDecorations(context) {
+    const decorationType = vscode.window.createTextEditorDecorationType({
+        color: '#3E9CD6' // 텍스트 색상을 파란색으로 설정
+    });
+
+    const updateDecorations = () => {
+        const activeEditor = vscode.window.activeTextEditor;
+        updateAtWordDecorations(activeEditor, decorationType);
+    };
+
+    if (vscode.window.activeTextEditor) {
+        updateDecorations(); // 초기 상태에서 decoration 업데이트
+    }
+
+    // Editor가 변경되었을 때 Decorations 업데이트
+    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateDecorations));
+
+    // 문서 내용이 변경되었을 때 Decorations 업데이트
+    context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(event => {
+        if (vscode.window.activeTextEditor && event.document === vscode.window.activeTextEditor.document) {
+            updateDecorations();
+        }
+    }));
+}
 
 // 매개변수 인덱스에 해당하는 라인의 코드를 가져온다.
 function getTextFromLine(lineIndex) {
